bf30d4bb5dd87c340cf989a7b472f1edcb2cfa11
Alexander Kuzmin <roosit@abricos.org>
2011-02-22 10:58:42 +0000

    Интернет-магазин: вывод списка каталога с корня

--- includes/catalog_list.php
+++ includes/catalog_list.php
@@ -20,18 +20,15 @@ $catalogManager = $mod->GetCatalogManager();
 
 $smMenu = CMSRegistry::$instance->modules->GetModule('sitemap')->GetManager()->GetMenu();
 $catItemMenu = null;
-$cnt = count($smMenu->menuLine);
-if ($cnt == 1){
-	$rootMenu = $smMenu->menuLine[0];
-	foreach ($rootMenu->child as $child){
-		if ($child->name == 'eshop'){
-			$catItemMenu = $child;
-			break;
-		}
+
+$rootMenu = $smMenu->menuLine[0];
+foreach ($rootMenu->child as $child){
+	if ($child->name == 'eshop'){
+		$catItemMenu = $child;
+		break;
 	}
-}else if ($cnt > 1){
-	$catItemMenu = $smMenu->menuLine[count($smMenu->menuLine)-1];
 }
+
 if (is_null($catItemMenu)){
 	$brick->content = "";
 	return;
@@ -63,84 +60,9 @@ foreach ($catItemMenu->child as $child){
 		"title" => addslashes(htmlspecialchars($child->source['tl'])),
 		"link" => $link
 	));
-	// print_r();
 }
 $brick->content = Brick::ReplaceVarByData($brick->content, array(
 	"result" => $lst
 ));
-
-/*
-while (($row = $db->fetch_array($rows))){
-	$el = $catalogManager->Element($row['id'], true);
-	if (empty($tempArr[$el['catid']])){
-		$tempArr[$el['catid']] = $smMenu->FindSource('id', $el['catid']);
-	}
-	$link = $tempArr[$el['catid']]->link;
-	// Проверка, является ли товар Новинкой, Акцией или Хитом продаж
-	$pr_spec = $el['fld_akc'] != 0 ? $brick->param->var["pr_akc"] : "";
-	$pr_spec .= $el['fld_new'] != 0 ? $brick->param->var["pr_new"] : "";
-	$pr_spec .= $el['fld_hit'] != 0 ? $brick->param->var["pr_hit"] : "";
-	$pr_spec11 = Brick::ReplaceVar($brick->param->var["pr_spec0"], "pr_spec", $pr_spec);
-
-	$imginfo = $db->fetch_array($catalogManager->FotoListThumb($el['elid'], $imgWidth, $imgHeight, 1));
-
-	if (empty($imginfo)){
-		$image = $brick->param->var["imgempty"];
-		$image = Brick::ReplaceVar($brick->param->var["imgempty"], "pr_spec1", $pr_spec11);
-	}else{
-		$thumb = CatalogModule::FotoThumbInfoParse($imginfo['thumb']);
-		
-		$image = Brick::ReplaceVarByData($brick->param->var["img"], array(
-			"src" => CatalogModule::FotoThumbLink($imginfo['fid'], $imgWidth, $imgHeight, $imginfo['fn']), 
-			"w" => ($thumb['w']>0 ? $thumb['w']."px" : ""),
-			"h" => ($thumb['h']>0 ? $thumb['h']."px" : ""),
-			"pr_spec1" => $pr_spec11
-		));
-	}
-
-	$lst .= Brick::ReplaceVarByData($brick->param->var['row'], array(
-		"tpl_btn" => $brick->param->var[$el['fld_sklad']==0 ? 'btnnotorder' : 'btnorder'],
-		"image" => $image, 
-		"title" => addslashes(htmlspecialchars($el['fld_name'])),
-		"price" => $el['fld_price'],
-		"link" => $link."product_".$row['id']."/",
-		"productid" => $row['id']
-	));
-}
-
-
-/*
-
-
-
-$listData = $mod->GetManager()->GetProductListData();
-$listPage = $listData['listPage'];
-if (intval($p['page'])>0){
-	$listPage = intval($p['page']);
-}
-
-$catids = $listData['catids'];
-
-$fld = $p['fld'];
-$perPage = bkint($p['count']);
-
-$brick->content = Brick::ReplaceVarByData($brick->content, array(
-	"page" => $listPage
-));
-
-$tempArr = array();
-
-//Проверяем наличие параметра fld у кирпича, вызывающего скрипт (выборка спецпредложений, новинок, акций и т.д.)
-if ($fld){
-	//выборка с сортировкой по полю параметра fld вызываемого кирпича базового типа товара
-	$rows = $catalogManager->ElementList(0, $listPage, $perPage, $fld);
-	// $rows = EShopQuery::ElementFldList($db, $fld, $listPage, $perPage);
-} else {
-	//выборка с сортировкой по полю fld_ord базового типа товара
-	// $rows = EShopQuery::ElementList($db, $catids, $listPage, $perPage);
-	$rows = $catalogManager->ElementList($catids, $listPage, $perPage);
-}
-
-/**/
 
 ?>
