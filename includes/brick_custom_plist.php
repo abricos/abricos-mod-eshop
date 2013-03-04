827efcff22a8a7c41a1000150e6fcfd2551df5c0
Alexander Kuzmin <roosit@abricos.org>
2013-03-04 13:47:49 +0400

    Добавлены параметр снятия ограничений на кол-во товаров по акции, хитам, новинкам

--- includes/brick_custom_plist.php
+++ includes/brick_custom_plist.php
@@ -1,26 +1,33 @@
 <?php
 /**
- * @version $Id$
  * @package Abricos
  * @subpackage EShop
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
- * @author Alexander Kuzmin (roosit@abricos.org)
+ * @author Alexander Kuzmin <roosit@abricos.org>
  */
 
 $brick = Brick::$builder->brick;
-$db = Abricos::$db;
 $p = &$brick->param->param;
-Abricos::GetModule('eshop');
-$mod = EShopModule::$instance;
 
-$listTotal = 0;
+$limit = 3;
+$more = $brick->param->var['more'];
 
-foreach ($brick->child as $child){
-	if ($child->name == 'product_list'){
-		$listTotal = $child->totalElementCount;
-	}
+if ($p['fulllist']){
+	$limit = 99;
+	$more = "";
 }
-if ($listTotal == 0){
+
+$brick->content = Brick::ReplaceVarByData($brick->content, array(
+		"more" => $more
+));
+
+$nbrick = Brick::$builder->LoadBrickS('eshop', 'product_list', $brick, array("p" => array(
+	"custwhere" => $p['custwhere'],
+	"count" => $limit
+)));
+
+if ($nbrick->totalElementCount == 0){
 	$brick->content = "";
 }
+
 ?>
