5d112688e69a401b95a1bfd62254e58cb647f3cc
Alexander Kuzmin <roosit@abricos.org>
2013-08-29 11:35:28 +0400

    Обновление инсталлятора

--- includes/shema.php
+++ includes/shema.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Схема таблиц данного модуля.
+ * Схема таблиц данного модуля
  * 
  * @package Abricos
  * @subpackage Eshop
@@ -13,24 +13,42 @@ $updateManager = Ab_UpdateManager::$current;
 $db = Abricos::$db;
 $pfx = $db->prefix;
 
-Abricos::GetModule('catalog')->GetManager();
+Abricos::GetModule('eshop')->GetManager();
+EShopManager::$instance->RoleDisable();
 
-Abricos::GetModule('eshop');
-$catalogManager = CatalogManager::$instance;
-$catalogManager->DisableRole();
+$cManager = EShopManager::$instance->cManager;
+
+function EShopOptionAppend($name, $title, $type, $size = ''){
+	$cManager = EShopManager::$instance->cManager;
+	
+	$d = new stdClass();
+	$d->nm = $name;
+	$d->tl = $title;
+	$d->tp = $type;
+	return $cManager->ElementOptionSave(0, $d);
+}
 
 if ($updateManager->isInstall()){
 	
-	// $catalogManager->ElementOptionAppend(0, 0, 3, 'name', 'Название', 'Название товара', 1, '{"cst":{"en":0,"inpen":0,"inp":"","onlden":0,"onld":""},"size":"255","def":""}');
-	$catalogManager->ElementOptionAppend(0, 0, 7, 'desc', 'Описание товара', '', 0, '{"cst":{"en":0,"inpen":0,"inp":"","onlden":0,"onld":""}}');  
-	$catalogManager->ElementOptionAppend(0, 0, 3, 'art', 'Артикул', '', 1, '{"cst":{"en":0,"inpen":0,"inp":"","onlden":0,"onld":""},"size":"32","def":""}');
-	$catalogManager->ElementOptionAppend(0, 0, 1, 'sklad', 'Количество на складе', '', 0, '{"cst":{"en":0,"inpen":0,"inp":"","onlden":0,"onld":""},"size":"5","def":"0"}');
-	$catalogManager->ElementOptionAppend(0, 0, 2, 'price', 'Цена розничная', '', 0, '{"cst":{"en":0,"inpen":0,"inp":"","onlden":0,"onld":""},"size":"10,2","def":"0"}');
-	$catalogManager->ElementOptionAppend(0, 0, 5, 'brand', 'Бренд', '', 0, '{"cst":{"en":0,"inpen":0,"inp":"","onlden":0,"onld":""}}');
-	$catalogManager->ElementOptionAppend(0, 0, 5, 'country', 'Страна-производитель', '', 0, '{"cst":{"en":0,"inpen":0,"inp":"","onlden":0,"onld":""}}');
-	$catalogManager->ElementOptionAppend(0, 0, 0, 'new', 'Новинка', '', 0, '{"cst":{"en":0,"inpen":0,"inp":"","onlden":0,"onld":""},"def":"0"}');
-	$catalogManager->ElementOptionAppend(0, 0, 0, 'hit', 'Хит продаж', '', 0, '{"cst":{"en":0,"inpen":0,"inp":"","onlden":0,"onld":""},"def":"0"}');
-	$catalogManager->ElementOptionAppend(0, 0, 1, 'akc', 'Акция', '', 0, '{"cst":{"en":0,"inpen":0,"inp":"","onlden":0,"onld":""},"size":"10","def":"0"}');
+	EShopOptionAppend('desc', 'Описание товара', Catalog::TP_TEXT);
+	EShopOptionAppend('art', 'Артикул', Catalog::TP_STRING, 32);
+	EShopOptionAppend('sklad', 'Количество на складе', Catalog::TP_NUMBER, 5);
+	EShopOptionAppend('price', 'Цена розничная', Catalog::TP_DOUBLE, '10,2');
+	EShopOptionAppend('brand', 'Бренд', Catalog::TP_TABLE);
+	EShopOptionAppend('country', 'Страна-производитель', Catalog::TP_TABLE);
+	EShopOptionAppend('new', 'Новинка', Catalog::TP_BOOLEAN);
+	EShopOptionAppend('hit', 'Хит продаж', Catalog::TP_BOOLEAN);
+	EShopOptionAppend('akc', 'Акция', Catalog::TP_NUMBER, 10);
+	
+	// $catalogManager->ElementOptionAppend(0, 0, 7, 'desc', 'Описание товара', '', 0, '{"cst":{"en":0,"inpen":0,"inp":"","onlden":0,"onld":""}}');  
+	// $catalogManager->ElementOptionAppend(0, 0, 3, 'art', 'Артикул', '', 1, '{"cst":{"en":0,"inpen":0,"inp":"","onlden":0,"onld":""},"size":"32","def":""}');
+	// $catalogManager->ElementOptionAppend(0, 0, 1, 'sklad', 'Количество на складе', '', 0, '{"cst":{"en":0,"inpen":0,"inp":"","onlden":0,"onld":""},"size":"5","def":"0"}');
+	// $catalogManager->ElementOptionAppend(0, 0, 2, 'price', 'Цена розничная', '', 0, '{"cst":{"en":0,"inpen":0,"inp":"","onlden":0,"onld":""},"size":"10,2","def":"0"}');
+	// $catalogManager->ElementOptionAppend(0, 0, 5, 'brand', 'Бренд', '', 0, '{"cst":{"en":0,"inpen":0,"inp":"","onlden":0,"onld":""}}');
+	// $catalogManager->ElementOptionAppend(0, 0, 5, 'country', 'Страна-производитель', '', 0, '{"cst":{"en":0,"inpen":0,"inp":"","onlden":0,"onld":""}}');
+	// $catalogManager->ElementOptionAppend(0, 0, 0, 'new', 'Новинка', '', 0, '{"cst":{"en":0,"inpen":0,"inp":"","onlden":0,"onld":""},"def":"0"}');
+	// $catalogManager->ElementOptionAppend(0, 0, 0, 'hit', 'Хит продаж', '', 0, '{"cst":{"en":0,"inpen":0,"inp":"","onlden":0,"onld":""},"def":"0"}');
+	// $catalogManager->ElementOptionAppend(0, 0, 1, 'akc', 'Акция', '', 0, '{"cst":{"en":0,"inpen":0,"inp":"","onlden":0,"onld":""},"size":"10","def":"0"}');
 }
 
 if ($updateManager->isUpdate('0.1.0.6')){
@@ -61,6 +79,7 @@ if ($updateManager->isUpdate('0.1.0.11') && !$updateManager->isInstall()){
 	$db->query_write("DROP TABLE IF EXISTS ".$pfx."eshp_discount");
 }
 
+/*
 if ($updateManager->isUpdate('0.1.0.12') && !$updateManager->isInstall()){
 	$db->query_write("
 		ALTER TABLE `".$pfx."eshp_order` ADD `deliveryid` int(10) unsigned NOT NULL DEFAULT 0 AFTER `userid`
@@ -70,6 +89,7 @@ if ($updateManager->isUpdate('0.1.0.12') && !$updateManager->isInstall()){
 	");
 	
 }
+/**/
 
 if ($updateManager->isUpdate('0.2')){
 	Abricos::GetModule('eshop')->permission->Install();
@@ -86,12 +106,6 @@ if ($updateManager->isUpdate('0.2.1') && !$updateManager->isInstall()){
 			metakeys = fld_metakeys,
 			metadesc = fld_metadesc 
 	");
-	
-	// $catalogManager->ElementOptionRemoveByName(0, 'name');
-	// $catalogManager->ElementOptionRemoveByName(0, 'ord');
-	// $catalogManager->ElementOptionRemoveByName(0, 'metatitle');
-	// $catalogManager->ElementOptionRemoveByName(0, 'metakeys');
-	// $catalogManager->ElementOptionRemoveByName(0, 'metadesc');
 	
 	$db->query_write("
 		DELETE FROM `".$pfx."ctg_eshp_eloption`
