8f809ca56dccbed70980a35edfb12a8fd30e7423
Alexander Kuzmin <roosit@abricos.org>
2014-02-26 14:21:10 +0400

    twitter typeahead search autocomplete release

--- src/includes/brick_js_search.php
+++ src/includes/brick_js_search.php
@@ -7,18 +7,24 @@
  */
 
 $cManager = EShopModule::$instance->GetManager()->cManager;
-$query = Abricos::CleanGPC('p', 'query', TYPE_STR);
-$extFilterField = Abricos::CleanGPC('p', 'eff', TYPE_STR);
-$extFilterValue = Abricos::CleanGPC('p', 'ef', TYPE_STR);
+$query = Abricos::CleanGPC('g', 'q', TYPE_STR);
+$extFilterField = Abricos::CleanGPC('g', 'eff', TYPE_STR);
+$extFilterValue = Abricos::CleanGPC('g', 'ef', TYPE_STR);
 
 header('Content-type: text/plain');
 
 $arr = $cManager->SearchAutoComplete($query, $extFilterField, $extFilterValue);
 
+$return = array();
+
 for ($i = 0; $i < count($arr); $i++) {
-    print($arr[$i]."\n");
+    $item = new stdClass();
+    $item->tl = $arr[$i];
+    $return[] = $item;
 }
 
+$json = json_encode($return);
+print_r($json);
 exit;
 
 ?>
